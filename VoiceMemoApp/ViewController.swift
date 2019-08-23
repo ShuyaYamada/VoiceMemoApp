240bd7828c35fc7e2e6054953b9d409b95181bb6
ShuyaYamada <syuya0219y@gmail.com>
2019-08-23 11:56:26 +0900

    Initial Commit

---
+++ VoiceMemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceMemoApp
+//
+//  Created by ShuyaYamada on 2019/08/23.
+//  Copyright © 2019 ShuyaYamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
